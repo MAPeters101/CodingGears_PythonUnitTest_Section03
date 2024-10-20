755407389eee38a628a8a449ab3390d90997df8a
Mark Peters <mapeters101@earthlink.net>
2024-10-20 14:21:40 -0400

    Lesson09: Work on first unit test case.

---
+++ test_calculator.py
@@ -0,0 +1,14 @@
+import unittest
+import calculator
+
+
+class TestCalculator(unittest.TestCase):
+    def test_add_functionality(self):
+        result = calculator.calc_add(10, 20)
+        self.assertEqual(result, 30)
+
+    def test_add_functionality_two_negative_numbers(self):
+        result = calculator.calc_add(-10, -20)
+        self.assertEqual(result, -30)
+
+
