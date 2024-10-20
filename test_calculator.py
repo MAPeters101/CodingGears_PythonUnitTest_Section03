7c0a706babc41daae561a6d65e41bc06d7dd9e1d
Mark Peters <mapeters101@earthlink.net>
2024-10-20 15:22:34 -0400

    Lesson11: Complete work on running test cases.

--- test_calculator.py
+++ test_calculator.py
@@ -12,3 +12,13 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(result, -30)
 
 
+class TestCalculator2(unittest.TestCase):
+    def test_add_functionality2(self):
+        result = calculator.calc_add(10, 20)
+        self.assertEqual(result, 30)
+
+    def test_add_functionality_two_negative_numbers2(self):
+        result = calculator.calc_add(-10, -20)
+        self.assertEqual(result, -30)
+
+
