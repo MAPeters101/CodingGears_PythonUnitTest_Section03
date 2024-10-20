dcf42a69d85507a77d64fd2d8d886a6b8737a3d5
Mark Peters <mapeters101@earthlink.net>
2024-10-20 16:58:33 -0400

    Lesson13: Complete work on unit test cases revisited.

--- unit_testing_01/test_calculator.py
+++ unit_testing_01/test_calculator.py
@@ -1,24 +1,24 @@
 import unittest
-import calculator
+import unit_testing_01.calculator
 
 
 class TestCalculator(unittest.TestCase):
     def test_add_functionality(self):
-        result = calculator.calc_add(10, 20)
+        result = unit_testing_01.calculator.calc_add(10, 20)
         self.assertEqual(result, 30)
 
     def test_add_functionality_two_negative_numbers(self):
-        result = calculator.calc_add(-10, -20)
+        result = unit_testing_01.calculator.calc_add(-10, -20)
         self.assertEqual(result, -30)
 
 
 class TestCalculator2(unittest.TestCase):
     def test_add_functionality2(self):
-        result = calculator.calc_add(10, 20)
+        result = unit_testing_01.calculator.calc_add(10, 20)
         self.assertEqual(result, 30)
 
     def test_add_functionality_two_negative_numbers2(self):
-        result = calculator.calc_add(-10, -20)
+        result = unit_testing_01.calculator.calc_add(-10, -20)
         self.assertEqual(result, -30)
 
 
